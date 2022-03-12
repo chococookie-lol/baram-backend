b40484e96b1af153e8361541065efcde8de0f067
dolphinlmg <dolphinlmg@naver.com>
2022-03-12 23:44:53 +0900

    Update db.js
    - use connection pool

--- model/db/db.js
+++ model/db/db.js
@@ -1,18 +1,18 @@
 const mysql = require('mysql');
 
-const {DB_HOST, DB_PORT, DB_USER, DB_PASSWORD, DB_NAME} = process.env;
+const { DB_HOST, DB_PORT, DB_USER, DB_PASSWORD, DB_NAME } = process.env;
 
-class Sql{
+class Sql {
     constructor() {
-        this.connection = mysql.createConnection({
+        this.connection = mysql.createPool({
             host: DB_HOST,
             port: DB_PORT,
             user: DB_USER,
             password: DB_PASSWORD,
-            database: DB_NAME 
+            database: DB_NAME
         })
 
-        this.connection.connect(err => {
+        this.connection.getConnection(err => {
             if (err) console.log("Mysql 연결 실패", err);
             else console.log("Mysql 연결 성공");
         })
@@ -21,10 +21,19 @@ class Sql{
     queryToDB(query) {
         console.log('query: ', query);
         return new Promise((resolve, reject) => {
-            this.connection.query(query, (error, rows, fields) => {
-                if (error) reject(error);
-                else resolve(rows);
-            });
+            this.connection.getConnection((err, connect) => {
+                if (err) {
+                    console.log(err);
+                    console.error(err);
+                    connect.release();
+                    return reject(err);
+                }
+                connect.query(query, (error, rows) => {
+                    connect.release;
+                    if (error) reject(error);
+                    else resolve(rows);
+                });
+            })
         });
     }
 }
